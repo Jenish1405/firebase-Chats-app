764c3eb77686a5125761d8cedf92a181586f00d0
Vaidika <vaidika@jacksolutions.co>
2023-01-09 10:24:30 +0530

    change styles

--- Src1/Screens/Home.js
+++ Src1/Screens/Home.js
@@ -94,7 +94,7 @@ export default Home
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'red'
+        backgroundColor: 'blue'
     },
     headercontainer: {
         height: 70,
